2ae30c22ce2c31ac4e69eb8a337b6b5a8109312c
umilosev <umilosev12822rn@raf.rs>
2025-03-11 23:44:16 +0100

    Make the method PUT instead of POST

--- src/api/cards.ts
+++ src/api/cards.ts
@@ -2,28 +2,27 @@ import { EmployeeCardResponseDto } from './response/cards';
 import { Axios } from 'axios';
 
 export const searchCards = async (
-    client: Axios,
-    filters: {
-      cardNumber: string;
-      firstName: string;
-      lastName: string;
-      email: string;
-      cardStatus: string;
-    },
-    rowsPerPage: number,
-    currentPage: number
-  ) => {
-    return client.get<EmployeeCardResponseDto>('/cards/employee/search', {
-      params: { ...filters, size: rowsPerPage, page: currentPage },
-    });
-  };
+  client: Axios,
+  filters: {
+    cardNumber: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    cardStatus: string;
+  },
+  rowsPerPage: number,
+  currentPage: number
+) => {
+  return client.get<EmployeeCardResponseDto>('/cards/employee/search', {
+    params: { ...filters, size: rowsPerPage, page: currentPage },
+  });
+};
 
 export const blockCard = async (client: Axios, cardNumber: string) =>
-    client.put<void>(`cards/block/${ cardNumber }` );
+  client.put<void>(`cards/block/${cardNumber}`);
 
 export const unblockCard = async (client: Axios, cardNumber: string) =>
-    client.put<void>(`cards/unblock/${ cardNumber }` );
+  client.put<void>(`cards/unblock/${cardNumber}`);
 
 export const deactivateCard = async (client: Axios, cardNumber: string) =>
-    client.put<void>(`cards/deactivate/${ cardNumber }` );
-  
+  client.put<void>(`cards/deactivate/${cardNumber}`);

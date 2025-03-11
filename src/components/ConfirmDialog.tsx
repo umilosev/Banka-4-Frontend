495e092c329a777f91aea2b5033eeb0cd90872a7
umilosev <umilosev12822rn@raf.rs>
2025-03-11 11:43:37 +0100

    Make ConfirmDialog previously known as DeleteDialog, it will allow us to be more dynamic with our dialogs in the future

---
+++ src/components/ConfirmDialog.tsx
@@ -0,0 +1,58 @@
+import { MaybePromise } from '@/types/MaybePromise';
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+
+export type ConfirmDialogProps = {
+  open: boolean;
+  onConfirm: () => MaybePromise<unknown>;
+  onCancel?: () => MaybePromise<unknown>;
+  title: string;
+  description: string;
+  buttonText: string;
+  itemName?: string;
+};
+
+export const ConfirmDialog = ({
+  open,
+  onConfirm,
+  onCancel,
+  title,
+  description,
+  buttonText,
+  itemName,
+}: ConfirmDialogProps) => {
+  return (
+    <AlertDialog open={open}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>
+            {description} {itemName ? `"${itemName}"` : 'this item'}? This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel asChild>
+            <Button type="button" variant="secondary" onClick={onCancel}>
+              Cancel
+            </Button>
+          </AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <Button onClick={onConfirm} variant="destructive">
+              {buttonText}
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};

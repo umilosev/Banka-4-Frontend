86db0a4f6767014b50f1e869b772c1c9f8cbdadb
umilosev <umilosev12822rn@raf.rs>
2025-03-11 23:29:29 +0100

    Implement the necessary API calls for editing the cards

---
+++ src/api/cards.ts
@@ -0,0 +1,29 @@
+import { EmployeeCardResponseDto } from './response/cards';
+import { Axios } from 'axios';
+
+export const searchCards = async (
+    client: Axios,
+    filters: {
+      cardNumber: string;
+      firstName: string;
+      lastName: string;
+      email: string;
+      cardStatus: string;
+    },
+    rowsPerPage: number,
+    currentPage: number
+  ) => {
+    return client.get<EmployeeCardResponseDto>('/cards/employee/search', {
+      params: { ...filters, size: rowsPerPage, page: currentPage },
+    });
+  };
+
+export const blockCard = async (client: Axios, cardNumber: string) =>
+    client.put<void>(`cards/block/${ cardNumber }` );
+
+export const unblockCard = async (client: Axios, cardNumber: string) =>
+    client.put<void>(`cards/unblock/${ cardNumber }` );
+
+export const deactivateCard = async (client: Axios, cardNumber: string) =>
+    client.put<void>(`cards/deactivate/${ cardNumber }` );
+  

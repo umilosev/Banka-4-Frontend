e18500414ca53cadd878fee2d8f467678ae6f70a
Marko Kocic <markokocic2000@gmail.com>
2025-03-13 01:21:55 +0100

    dialog: bring back DeleteDialog

---
+++ src/components/DeleteDialog.tsx
@@ -0,0 +1,53 @@
+import { MaybePromise } from '@/types/MaybePromise';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+
+export type DeleteDialogProps = {
+  open: boolean;
+  onConfirm: () => MaybePromise<unknown>;
+  onCancel?: () => MaybePromise<unknown>;
+  itemName?: string;
+};
+
+export const DeleteDialog = ({
+  open,
+  onConfirm,
+  onCancel,
+  itemName,
+}: DeleteDialogProps) => {
+  return (
+    <AlertDialog open={open}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Confirm Deletion</AlertDialogTitle>
+          <AlertDialogDescription>
+            Are you sure you want to delete{' '}
+            {itemName ? `"${itemName}"` : 'this item'}? This action cannot be
+            undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel asChild>
+            <Button type="button" variant="secondary" onClick={onCancel}>
+              Cancel
+            </Button>
+          </AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <Button onClick={onConfirm} variant="destructive">
+              Delete
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
